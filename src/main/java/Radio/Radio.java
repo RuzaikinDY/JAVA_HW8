d5bcf122c2dcd6d770bf6543922f77ab4a0d0f14
RuzaikinDY <Devilwind@mail.ru>
2022-06-12 19:33:48 +0300

    add lombok

--- src/main/java/Radio/Radio.java
+++ src/main/java/Radio/Radio.java
@@ -1,5 +1,13 @@
 package Radio;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
 public class Radio {
     private int currentStation;
 
@@ -13,17 +21,10 @@ public class Radio {
 
     private int maxVolume = 100;
 
-    public Radio(int maxStation){
-        this.maxStation= maxStation;
+    public Radio(int maxStation) {
+        this.maxStation = maxStation;
     }
 
-    public Radio() {
-
-    }
-
-    public int getCurrentStation() {
-        return currentStation;
-    }
 
     public void setCurrentStation(int currentStation) {
         if (currentStation > maxStation) {
@@ -54,9 +55,6 @@ public class Radio {
         currentStation--;
     }
 
-    public int getCurrentVolume() {
-        return currentVolume;
-    }
 
     public void setCurrentVolume(int currentVolume) {
         if (currentVolume > maxVolume) {
@@ -81,29 +79,6 @@ public class Radio {
             currentVolume--;
         }
     }
-    public int getMaxStation () {
-        return maxStation;
-    }
-    public void setMaxStation(int maxStation) {
-        this.maxStation = maxStation;
-    }
-    public int getMinStation() {
-        return minStation;
-    }
-    public void setMinStation(int minStation) {
-        this.minStation = minStation;
-    }
-    public int getMaxVolume() {
-        return maxVolume;
-    }
-    public void setMaxVolume(int maxVolume) {
-        this.maxVolume = maxVolume;
-    }
-    public int getMinVolume() {
-        return minVolume;
-    }
-    public void setMinVolume(int minVolume) {
-        this.minVolume = minVolume;
-    }
 }
+
 
